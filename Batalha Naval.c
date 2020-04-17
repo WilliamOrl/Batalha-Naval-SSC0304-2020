1f5aaa169d5cbb5f5b307774d73804bd45d2accd
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-04-17 11:37:43 -0300

    Create Batalha Naval.c

---
+++ Batalha Naval.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+	int aux;
+	
+	printf("Bem Vindo a Batalha Naval!\n");		//inicio do programa 
+	printf("Precione:\n 1-Iniciar \n2-Ajuda\n3-Sair\n");
+	scanf("%d",aux);
+	
+	switch (aux){
+		case 1:
+			
+		break;
+		
+		case 2:
+		
+		break;
+		
+		case 3:
+			
+		break;	
+	}
+	
+	
+}
