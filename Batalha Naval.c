1e50331cf243e6fa5dbe2ed87716de9760c21fd3
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-04-20 13:11:59 -0300

    Update Batalha Naval.c

--- Batalha Naval.c
+++ Batalha Naval.c
@@ -5,12 +5,14 @@
 #define T 7			// Torpedeiros		qtd.3
 #define H 8			// Hidroavioes		qtd.4
 
-int iniciar_matrizes(int **campo1,int **campo2);
+void  iniciar_matrizes(int campo1[16][16],int campo2 [16][16]);
+void Ajuda(void);
 
 int main(void){
 	int aux;
 	int campo1[16][16],campo2[16][16];
-	
+
+inicio:	
 	printf("Bem Vindo a Batalha Naval!\n");		//inicio do programa 
 	printf("Precione:\n1-Iniciar \n2-Ajuda\n3-Sair\n");
 	scanf("%d",&aux);
@@ -22,18 +24,37 @@ int main(void){
 		break;
 		
 		case 2:
-		
+			Ajuda();
+			goto inicio;
 		break;
 		
 		case 3:
 			
 		break;	
 	}
-	
-	int iniciar_matrizes(int **campo1,int **campo2){
+}
+
+void iniciar_matrizes(int campo1[16][16],int campo2[16][16]){
+		
 		
 		
 	}
 	
-	
+void Ajuda(void){
+	char c;
+	system("cls");
+	printf("Reset - Nova partida\n");
+	printf("Sair - Abandonar o Jogo\n");
+	printf("Ajuda-Retona a este Menu\n");
+	printf("Acaso - Inicia um novo tabuleiro mantendo os pontos\n");
+	printf("Gravar (nome do arquivo) - Salva o jogo\n");
+	printf("Carregar (nome do arquivo) - Carrega o jogo\n");
+	printf("Pressecione qualquer tecla para sair:");
+	while(1){
+		c =	getchar();
+		if(c!='\n'){
+			system("cls");
+			return;
+		}
+	}
 }
